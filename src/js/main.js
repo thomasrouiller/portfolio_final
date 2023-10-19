9da504378db3c533511c707cc6a512a95579d08f
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-10-19 14:04:47 +0200

    ok i'll finish later

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,35 @@ import gsap from "gsap";
 // lazyImages.forEach((img) => {
 //   observer.observe(img);
 // });
+
+// var button = document.querySelector(".navigation-close");
+// var menu = document.querySelector(".navigation-open");
+// var navigation = document.querySelector(".navigation-credits-informations");
+
+// var openMenu = function () {
+//   menu.classList.toggle("is-open");
+//   button.classList.toggle("is-active");
+//   navigation.classList.toggle("is-open");
+// };
+
+// button.addEventListener("click", openMenu);
+
+// links.forEach((link) => {
+//   link.addEventListener("click", openMenu);
+// });
+
+var button = document.querySelector(".navigation-close");
+var menu = document.querySelector(".navigation-open");
+var navigation = document.querySelector(".navigation-credits-informations");
+
+var openMenu = function () {
+  menu.classList.toggle("is-open");
+  button.classList.toggle("is-active");
+  navigation.classList.toggle("is-open");
+};
+
+button.addEventListener("click", openMenu);
+
+links.forEach((link) => {
+  link.addEventListener("click", openMenu);
+});
